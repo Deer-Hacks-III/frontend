8556b7200589d986624b2f36b6b29c8e75625823
abdooodi <156738379+abdooodi@users.noreply.github.com>
2024-02-18 01:47:45 -0500

    Update InfoBox.py

--- InfoBox.py
+++ InfoBox.py
@@ -1,62 +1,86 @@
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets, QtMultimedia, QtMultimediaWidgets
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QImage, QColor, QPixmap, QIcon, QFont
-from PyQt5.QtWidgets import (QApplication, QDialog, QMainWindow, QLabel, \
-    QSplashScreen, QMenu, QAction, QWidget, QVBoxLayout, \
-    QComboBox, QPushButton, QMessageBox, QGridLayout, QLineEdit, QHBoxLayout,
-                             QStackedLayout)
-from barcode_scanner import QRScanner
-from Database import UPCManager, UPCManagerLocal
-from list import ListScreen, ListElement
+from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtGui import QPixmap, QImage, QColor
+import requests
+import Database
 
-class MainApplication(QDialog):
+class ProductInformation(QDialog):
+    def __init__(self, item, parent=None):
+        self.item = item
+        product_name = item.get_name()
+        product_image_url = item.get_img()
+        product_score = item.get_score()
+        super(ProductInformation, self).__init__(parent)
+        self.setWindowTitle("Product Information")
+        self.layout = QVBoxLayout()
 
-    def __init__(self, parent = None):
-        super().__init__(parent)
-        self.setWindowTitle("Project Name")
-        self.main_layout = QVBoxLayout()
-        self.setGeometry(100, 100, 800, 650)
-        # Scan layouts
-        self.current_window = QStackedLayout()
-        self.scan_button = QPushButton(f"Scanning")
-        self.scan_button.clicked.connect(self.set_scan_layout)
+        # Product Name
+        self.product_name_label = QLabel(f"<b>Product Name:</b> {product_name}")
+        self.layout.addWidget(self.product_name_label)
 
-        # Shopping Layouts
-        self.list_button = QPushButton(f"Shopping List")
-        self.list_button.clicked.connect(self.set_list_layout)
+        # Product Image
+        self.product_image = QLabel()
+        self.load_image_from_url(product_image_url)
+        self.layout.addWidget(self.product_image)
 
-        self.bottom_layout = QHBoxLayout()
-        self.bottom_layout.addWidget(self.list_button)
-        self.bottom_layout.addWidget(self.scan_button)
-        # By default, be at the scan layout
+        # Product Score
+        self.product_score_label = QLabel(f"<b>Product Score:</b> {product_score}")
+        self.layout.addWidget(self.product_score_label)
+        
+        # Add an "add to list" button
+        self.add_to_list_button = QPushButton("Add to List")
+        self.layout.addWidget(self.add_to_list_button)
+        self.add_to_list_button.clicked.connect(self.add_to_list)
+        # add a "view alternatives" button
+        self.view_alternatives_button = QPushButton("View Alternatives")
+        self.layout.addWidget(self.view_alternatives_button)
+        
+        
+        # Set score color
+        # Convert product_score into ascii value
+        try:
+            product_score = product_score.upper()
+            ascii = ord(product_score)
+            # Make a very green color 
+            green = QColor(0, 255, 100)
+            # For every ascii value above ord(a), add red
+            ascii -= ord('A')
+            ascii *= 50
+            green.setRed(ascii)
+        except:
+            green = QColor(128, 128, 150)
+        self.setStyleSheet(f"background-color: {green.name()}")
 
-        self.current_window.addWidget(ListScreen(database))
-        self.current_window.addWidget(QRScanner())
-        self.current_window.setCurrentIndex(0)
+        self.setLayout(self.layout)
 
-        self.main_layout.addLayout(self.current_window)
-        self.main_layout.addLayout(self.bottom_layout)
-        self.setLayout(self.main_layout)
+    def add_to_list(self):
+        db = Database.UPCManager("http://127.0.0.1:5000")
+        db.add_upc(self.item.get_upc())
+        self.close()
 
+    def load_image_from_url(self, url):
+        response = requests.get(url)
+        image = QImage()
+        image.loadFromData(response.content)
+        pixmap = QPixmap.fromImage(image)
+        self.product_image.setPixmap(pixmap)
 
-    def set_scan_layout(self) -> None:
-        self.current_window.setCurrentIndex(1)
-        self.update()
+    def get_score_color(self, score):
+        # Set color based on score
+        if score >= 9.0:
+            return "#2E8B57"  # Dark Green for A
+        elif score >= 8.0:
+            return "#3CB371"  # Medium Sea Green for B
+        elif score >= 7.0:
+            return "#8FBC8F"  # Dark Sea Green for C
+        else:
+            return "#DC143C"  # Crimson for D and below
 
-    def set_list_layout(self) -> None:
-        self.current_window.setCurrentIndex(0)
-        # Get the ListScreen
-        self.current_window.widget(0).update_list(database.get_all_upcs())
-        self.update()
-
-
-
-if __name__ == "__main__":
-    database = UPCManager("http://127.0.0.1:5000")
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True) #enable highdpi scaling
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True) #use highdpi icons
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    program = MainApplication()
-    program.exec()
-    sys.exit(app.exec_())
+    product_name = "Product A"
+    product_brand = "Brand X"
+    product_image_url = "https://avatars.githubusercontent.com/u/62670577?v=4"
+    product_score = "A"
+    product_info = ProductInformation(product_name, product_image_url, product_score)
+    product_info.exec_()
