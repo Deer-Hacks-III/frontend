db5bf6806600a68159c298292241cb2a0e140570
abdooodi <156738379+abdooodi@users.noreply.github.com>
2024-02-18 05:56:08 -0500

    Update improved_menu.py

--- improved_menu.py
+++ improved_menu.py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import QImage, QColor, QPixmap, QIcon, QFont
 from PyQt5.QtWidgets import (QApplication, QDialog, QMainWindow, QLabel, \
     QSplashScreen, QMenu, QAction, QWidget, QVBoxLayout, \
     QComboBox, QPushButton, QMessageBox, QGridLayout, QLineEdit, QHBoxLayout,
-                             QStackedLayout)
+                             QStackedLayout, QScrollArea)
 from barcode_scanner import QRScanner
 from Database import UPCManager, UPCManagerLocal
 from list import ListScreen, ListElement
@@ -14,58 +14,61 @@ class MainApplication(QDialog):
 
     def __init__(self, parent = None):
         super().__init__(parent)
-        self.setWindowTitle("Project Name")
+        self.setWindowTitle("GreenCart")
         self.main_layout = QVBoxLayout()
-        self.setGeometry(100, 100, 800, 650)
+        self.setGeometry(0, 0, 800, 650)
         # Scan layouts
         self.current_window = QStackedLayout()
-        self.scan_button = QPushButton(f"Scanning")
+        self.scan_button = QPushButton("Scanning")
+        self.scan_button.setIcon(QIcon('icons/barcode.svg'))
+        self.scan_button.setFixedHeight(50)
+        self.scan_button.setFixedWidth(400)
         self.scan_button.setStyleSheet("QPushButton {background-color: rgb(217,237,247); border: 2px solid rgb(83, 115, 145); border-radius: 10px; padding: 5px;} \
-                                    QPushButton:hover {background-color: rgb(175,217,238); border: 2px solid rgb(42, 82, 120);}")
+                                    QPushButton:hover {background-color: rgb(175,175,255); border: 2px solid rgb(42, 82, 120);}")
         self.scan_button.clicked.connect(self.set_scan_layout)
 
         # Shopping Layouts
-        self.list_button = QPushButton(f"Shopping List")
+
+        self.list_button = QPushButton("Shopping List")
+        self.list_button.setIcon(QIcon('icons/shoplist.svg'))
+        self.list_button.setFixedHeight(50)
+        self.list_button.setFixedWidth(400)
         self.list_button.setStyleSheet("QPushButton {background-color: rgb(217,237,247); border: 2px solid rgb(83, 115, 145); border-radius: 10px; padding: 5px;} \
-                                    QPushButton:hover {background-color: rgb(175,217,238); border: 2px solid rgb(42, 82, 120);}")
+                                    QPushButton:hover {background-color: rgb(150,255,200); border: 2px solid rgb(42, 82, 120);}")
         self.list_button.clicked.connect(self.set_list_layout)
 
         self.bottom_layout = QHBoxLayout()
         self.bottom_layout.addWidget(self.list_button)
         self.bottom_layout.addWidget(self.scan_button)
+        self.bottom_layout.setAlignment(Qt.AlignBottom)
         # By default, be at the scan layout
-
         self.current_window.addWidget(ListScreen(database))
-        self.current_window.addWidget(QRScanner())
+        scanner = QRScanner()
+        self.current_window.addWidget(scanner)
+        scanner.stop_camera()
         self.current_window.setCurrentIndex(0)
 
         self.main_layout.addLayout(self.current_window)
         self.main_layout.addLayout(self.bottom_layout)
         self.setStyleSheet("MainApplication {background-color: rgb(230, 230, 250);}")
         self.setLayout(self.main_layout)
+    
+
 
     def set_scan_layout(self) -> None:
+        if self.current_window.currentIndex() != 1:
+            self.current_window.widget(1).start_camera()
         self.current_window.setCurrentIndex(1)
-        #self.current_window = self.scan_layout
-        #self.update_layout()
+        self.update()
 
     def set_list_layout(self) -> None:
+        if self.current_window.currentIndex() != 0:
+            self.current_window.widget(1).stop_camera()
         self.current_window.setCurrentIndex(0)
         # Get the ListScreen
         self.current_window.widget(0).update_list(database.get_all_upcs())
-        #self.current_window = self.list_layout
-        #self.update_layout()
-
-    def update_layout(self) -> None:
-        self.main_layout = QVBoxLayout()
-        self.current_window.update()
-        self.list_layout
-        self.main_layout.addLayout(self.current_window)
-
-        self.main_layout.addLayout(self.bottom_layout)
-
-        self.setLayout(self.main_layout)
         self.update()
+
 
 
 if __name__ == "__main__":
@@ -74,5 +77,6 @@ if __name__ == "__main__":
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True) #use highdpi icons
     app = QApplication(sys.argv)
     program = MainApplication()
-    program.exec()
+    program.show()
+    #program.exec()
     sys.exit(app.exec_())
